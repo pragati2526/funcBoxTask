6ab6aa24be6ce25c7821e8a221bb2a7924f6528b
pragati2526 <spragati126@gmail.com>
2021-09-27 00:56:22 +0530

    routing fixed

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <div >
         
         <Switch>
-          <Route exact path="/funcBoxTask">
+          <Route exact path="/funcBoxTask/">
           <Navbar/>
             <LandingPage/>
             <Footer/>
